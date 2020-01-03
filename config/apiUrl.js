059f530b73312e0f1f9e30f1817a487db9dd6762
zhy <1144567324@qq.com>
2020-01-03 14:57:54 +0800

    1

--- config/apiUrl.js
+++ config/apiUrl.js
@@ -1,4 +1,4 @@
-let ipUrl = 'http://127.0.0.1:7001/default/'
+let ipUrl = 'http://127.0.0.1:12580/default/'
 let servicePath = {
     getArticalList: ipUrl + 'getArticleList', //  首页文章列表接口
     getArticalById: ipUrl + 'getArticleById/',  // 文章详细页内容接口 ,需要接收参数
